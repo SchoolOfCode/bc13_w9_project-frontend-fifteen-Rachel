1c3c50322d4b7bb82847587365e2af3c58101c38
Barbara Millon <millon.barbara1@gmail.com>
2022-11-22 15:34:35 +0000

    component tree

---
+++ front/src/components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+// import ReturnDataField from './components/ReturnDataField';
+import Input from '../Input/Input';
+import ResultsData from '../ResultsData/ResultsData';
+
+
+
+
+function App() {
+  return (
+   <>
+     <Input />
+     <ResultsData />
+       
+   </>
+  );
+}
+
+export default App;
